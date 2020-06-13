c0cf573ae2360590a81ed14b1ce6e239f399604b
prathameshsi19 <64113323+prathameshsi19@users.noreply.github.com>
2020-06-13 15:02:59 +1000

    Add files via upload

---
+++ build/precache-manifest.465750e5f51d4167dce69bd9a80158cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a158abc5987ab2bf6cb697b6cb1940",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b8d9f9ef4d6bcb59c90",
+    "url": "/static/css/main.a9aad952.chunk.css"
+  },
+  {
+    "revision": "18d026ee4608f80c264b",
+    "url": "/static/js/2.6d9f8b59.chunk.js"
+  },
+  {
+    "revision": "2484cd6bcea4995b92934619b09cb02d",
+    "url": "/static/js/2.6d9f8b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b8d9f9ef4d6bcb59c90",
+    "url": "/static/js/main.4fc3f184.chunk.js"
+  },
+  {
+    "revision": "5748d411c29cb57dc8d5",
+    "url": "/static/js/runtime-main.3238e42b.js"
+  },
+  {
+    "revision": "8aea2cbc925888af53230b77a3216ab9",
+    "url": "/static/media/user.8aea2cbc.svg"
+  }
+]);
